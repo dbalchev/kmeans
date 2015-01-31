1822d2a228ab2074eefccd8b4e3eae746f7740a4
Dani <dbalchev@gmail.com>
2015-01-31 13:52:39 +0200

    fixed euclidean distance

--- kmeans/distances.py
+++ kmeans/distances.py
@@ -1,33 +1,45 @@
 from .utils import count_duplicates
+from math import sqrt
 
+def get_norm_len(vec):
+    cu = 0
+    res = 0
+    while cu < len(vec):
+        cnt = count_duplicates(vec, cu)
+        res += cnt * cnt
+        cu += cnt
+    return res
+
+def dot_product(lh, rh):
+    result = 0
+    if len(lh) == 0 or len(rh) == 0:
+        return 0
+    li = 0
+    ri = 0
+    while li < len(lh) or ri < len(rh):
+        if li < len(lh) and (ri >= len(rh) or lh[li] < rh[ri]):
+            cnt = count_duplicates(lh, li)
+            li += cnt
+            continue
+        if ri < len(rh) and (li >= len(lh) or lh[li] > rh[ri]):
+            cnt = count_duplicates(rh, ri)
+            ri += cnt
+            continue
+        left_count = count_duplicates(lh, li)
+        right_count = count_duplicates(rh, ri)
+        result += left_count * right_count
+        li += left_count
+        ri += right_count
+    return result
 
 def euclidean_distance(lh, rh):
     """
     Прави нормализирано евклидово разстояние на lh и rh;
     lh и rh трябва да са сортиран вектор от индекси на думи
     """
-    if len(lh) == 0 or len(rh) == 0:
-        return 0
-    li = 0
-    ri = 0
-    nominator = 0
-    while li < len(lh) or ri < len(rh):
-        if li < len(lh) and (ri >= len(rh) or lh[li] < rh[ri]):
-            cnt = count_duplicates(lh, li)
-            nominator += cnt * cnt
-            li += cnt
-            continue
-        if ri < len(rh) and (li >= len(lh) or lh[li] > rh[ri]):
-            cnt = count_duplicates(rh, ri)
-            nominator += cnt * cnt
-            ri += cnt
-            continue
-        left_count = count_duplicates(lh, li)
-        right_count = count_duplicates(rh, ri)
-        delta = left_count - right_count
-        nominator += delta * delta
-        li += left_count
-        ri += right_count
 
-    denominator = len(lh) + len(rh)
-    return nominator / denominator
+    denominator = sqrt(get_norm_len(lh) * get_norm_len(rh))
+    if not denominator:
+        return 0
+    nominator = dot_product(lh, rh)
+    return 1 - nominator / denominator
--- kmeans/tests.py
+++ kmeans/tests.py
@@ -1,8 +1,7 @@
 import unittest
 
 from .utils import count_duplicates
-from .distances import euclidean_distance
-
+from .distances import euclidean_distance, dot_product
 
 class EuclideanDistanceTest(unittest.TestCase):
     def test_count_dup(self):
@@ -12,6 +11,23 @@ class EuclideanDistanceTest(unittest.TestCase):
         self.assertEqual(count_duplicates([1, 1, 1], 0), 3, "all same")
         self.assertEqual(count_duplicates([1, 1, 1], 1), 2, "all same indx != 0")
 
+    def test_dot(self):
+        tests = [
+            ([], [1,2,3], 0, "Empty sequence"),
+            ([1, 2, 3], [1, 2, 3], 3, "Same sequence"),
+            ([1, 1, 1], [1, 1, 1], 9, "Same sequnece repeating elements"),
+            ([1], [2], 0, "Nonintersecting sequences"),
+            ([1, 3, 5], [2, 4, 6], 0, "Nonintersecting longer sequences"),
+            ([1, 2], [1, 3], 1, ""),
+            ([1,1,1,1], [2,2,2], 0, "")
+        ]
+        for lh, rh, result, message in tests:
+            r1 = dot_product(lh, rh)
+            r2 = dot_product(rh, lh)
+            message = message or "{}, {}".format(lh, rh)
+            self.assertEqual(r1, r2, message)
+            self.assertEqual(r1, result, message)
+
     def test_euclidean_distance(self):
         tests = [
             ([], [1,2,3], 0, "Empty sequence"),
@@ -20,6 +36,7 @@ class EuclideanDistanceTest(unittest.TestCase):
             ([1], [2], 1, "Nonintersecting sequences"),
             ([1, 3, 5], [2, 4, 6], 1, "Nonintersecting longer sequences"),
             ([1, 2], [1, 3], 0.5, ""),
+            ([1,1,1,1], [2,2,2], 1, "")
         ]
         for lh, rh, result, message in tests:
             r1 = euclidean_distance(lh, rh)
